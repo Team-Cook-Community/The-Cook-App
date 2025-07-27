4b517db33e2b28427d8d3a87bb157028a82052b6
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-07-27 09:47:30 -0400

    fix: resolve overflow issue with flexbox

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HeroUIProvider className='size-full'>
+    <HeroUIProvider className='size-full flex flex-col'>
       <App />
     </HeroUIProvider>
   </StrictMode>,
