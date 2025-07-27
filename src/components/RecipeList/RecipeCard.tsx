2591375a8e1e5e37c07756d25b1904e835c2d0a7
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-07-27 14:16:22 -0400

    feat: redirect to the full recipe page when the card is pressed

--- src/components/RecipeList/RecipeCard.tsx
+++ src/components/RecipeList/RecipeCard.tsx
@@ -1,11 +1,24 @@
+import { useNavigate } from "react-router";
+
 import { Card, CardBody, CardFooter, Image, User } from "@heroui/react";
 import { HeartIcon } from "@phosphor-icons/react";
 
 export default function RecipeCard({ recipe }: any) {
+  const navigate = useNavigate();
+
+  const handlePress = () => {
+    navigate(`/recipe/${recipe.id}`);
+  };
+
   return (
-    <Card isPressable shadow="none" className="w-full mb-2!">
+    <Card
+      isPressable
+      shadow="none"
+      className="w-full mb-2!"
+      onPress={handlePress}
+    >
       <CardBody className="p-0">
-        <Image src={recipe.coverImg} alt="recipe" isZoomed/>
+        <Image src={recipe.coverImg} alt="recipe" isZoomed />
       </CardBody>
       <CardFooter className="flex-col gap-3">
         <h1 className="w-full text-left text-lg font-bold">{recipe.title}</h1>
@@ -14,11 +27,11 @@ export default function RecipeCard({ recipe }: any) {
             name={recipe.creator.firstName}
             description={recipe.creator.lastName}
             avatarProps={{
-                className: "w-[1.75rem] h-[1.75rem]",
+              className: "w-[1.75rem] h-[1.75rem]",
             }}
           />
           <div className="flex gap-1 items-center text-sm">
-            <HeartIcon size={15} weight="fill" color="#DC143C"/>
+            <HeartIcon size={15} weight="fill" color="#DC143C" />
             {recipe.likes > 999 ? "999+" : recipe.likes}
           </div>
         </div>
