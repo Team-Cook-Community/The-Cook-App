fe554ba2edb7acb5bd7ff01494ad30ad42bc53dc
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-07-27 17:39:49 -0400

    feat: set basename for browser router

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Router from "@routes/Router";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Layout>
         <Router />
       </Layout>
