a358855edab164213f250aace5e7e259bf01e7ee
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-07-27 14:13:22 -0400

    feat: create a component to display the full recipe

---
+++ src/components/RecipeViewer/index.tsx
@@ -0,0 +1,31 @@
+import { Image, ScrollShadow } from "@heroui/react";
+import PostCard from "@components/PostCard";
+import StepCard from "./StepCard";
+
+export default function RecipeViewer({ recipe }: any) {
+  return (
+    <ScrollShadow className="size-full" hideScrollBar>
+      <article className="w-full h-full flex flex-col gap-3">
+        <Image
+          src={recipe.coverImg}
+          alt={recipe.title}
+          className="object-cover"
+          width={"100%"}
+          height={250}
+          loading="lazy"
+          radius="none"
+        />
+        <div>
+          <PostCard creator={recipe.creator} shadow="none" radius="none" className="p-1">
+            {recipe.desc}
+          </PostCard>
+        </div>
+        <div className="flex flex-col gap-5 p-1">
+          {recipe.steps.map((step: any) => (
+            <StepCard step={step} />
+          ))}
+        </div>
+      </article>
+    </ScrollShadow>
+  );
+}
