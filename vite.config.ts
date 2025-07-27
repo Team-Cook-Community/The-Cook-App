45f30282c9834480384aa86a5fac37b8c4eaa559
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-07-27 09:53:35 -0400

    build: update tsconfig and Vite aliases for cleaner imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
+  },
 });
