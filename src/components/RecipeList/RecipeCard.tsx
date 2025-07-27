5fee7cf16b2983fd7d37b00cfbd88f9c11e7a1f0
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-07-27 12:06:07 -0400

    feat: implement the recipe list section of the home page

---
+++ src/components/RecipeList/RecipeCard.tsx
@@ -0,0 +1,28 @@
+import { Card, CardBody, CardFooter, Image, User } from "@heroui/react";
+import { HeartIcon } from "@phosphor-icons/react";
+
+export default function RecipeCard({ recipe }: any) {
+  return (
+    <Card isPressable shadow="none" className="w-full mb-2!">
+      <CardBody className="p-0">
+        <Image src={recipe.coverImg} alt="recipe" />
+      </CardBody>
+      <CardFooter className="flex-col gap-3">
+        <h1 className="w-full text-left text-lg font-bold">{recipe.title}</h1>
+        <div className="flex justify-between w-full items-center">
+          <User
+            name={recipe.creator.firstName}
+            description={recipe.creator.lastName}
+            avatarProps={{
+                className: "w-[1.75rem] h-[1.75rem]",
+            }}
+          />
+          <div className="flex gap-1 items-center text-sm">
+            <HeartIcon size={15} weight="fill" color="#DC143C"/>
+            {recipe.likes > 999 ? "999+" : recipe.likes}
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ src/data/recipe.ts
@@ -0,0 +1,83 @@
+//This is only for the demo purposes
+
+import JapanesePorkCutlet from "@assets/images/demo/recipe-covers/japanese-pork-cutlet.jpg";
+import CocaColaWings from "@assets/images/demo/recipe-covers/coca-cola-wings.jpg";
+import PizzaDiavola from "@assets/images/demo/recipe-covers/pizza-diavola.jpg";
+import RoastedPorkBelly from "@assets/images/demo/recipe-covers/roasted-pork-belly.jpg";
+import SteamedChineseCabbageInSupremeSoup from "@assets/images/demo/recipe-covers/steamed-chinese-cabbage-in-supreme-soup.jpg";
+import MapoTofu from "@assets/images/demo/recipe-covers/mapo-tofu.jpg";
+
+export const recipeList = [
+  {
+    id: 100361496,
+    title: "Japanese-style Fried Pork Cutlet",
+    coverImg: JapanesePorkCutlet,
+    tags: ["Japanese"],
+    creator: {
+      avatar: null,
+      firstName: "John",
+      lastName: "Doe",
+    },
+    likes: 200,
+  },
+  {
+    id: 1000229,
+    title: "Coca Cola Chicken Wings",
+    coverImg: CocaColaWings,
+    tags: ["Chinese"],
+    creator: {
+      avatar: null,
+      firstName: "Zheng",
+      lastName: "Ying",
+    },
+    likes: 666,
+  },
+  {
+    id: 104641037,
+    title: "Pizza Diavola",
+    coverImg: PizzaDiavola,
+    tags: ["Italian"],
+    creator: {
+      avatar: null,
+      firstName: "Judy",
+      lastName: "Zhu",
+    },
+    likes: 123,
+  },
+  {
+    id: 100027300,
+    title: "Roasted Pork Belly",
+    coverImg: RoastedPorkBelly,
+    tags: ["Italian"],
+    creator: {
+      avatar: null,
+      firstName: "Gordan",
+      lastName: "Ramsay",
+    },
+    likes: 999,
+  },
+  {
+    id: 10660918,
+    title: "Steamed Chinese Cabbage in Supreme Soup",
+    coverImg: SteamedChineseCabbageInSupremeSoup,
+    tags: ["Chinese"],
+    creator: {
+      avatar: null,
+      firstName: "Gang",
+      lastName: "Wang",
+    },
+    likes: 1996,
+  },
+  {
+    id: 78140,
+    title: "Mapo Tofu",
+    coverImg: MapoTofu,
+    tags: ["Chinese"],
+    creator: {
+      avatar: null,
+      firstName: "Gang",
+      lastName: "Wang",
+    },
+    likes: 592,
+  },
+];
