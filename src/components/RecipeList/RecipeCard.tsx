16c011b7feb4b8ee8af32186746e9fb57d17c58c
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-07-31 14:12:52 -0400

    feat: display skeleton if the data (e.g. image) is loaded yet

--- src/components/RecipeList/RecipeCard.tsx
+++ src/components/RecipeList/RecipeCard.tsx
@@ -1,13 +1,26 @@
 import { useNavigate } from "react-router";
+import { useState } from "react";
 
-import { Card, CardBody, CardFooter, Image, User } from "@heroui/react";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  User,
+  Skeleton,
+} from "@heroui/react";
 import { HeartIcon } from "@phosphor-icons/react";
 
 export default function RecipeCard({ recipe }: any) {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
 
   const handlePress = () => {
     navigate(`/recipe/${recipe.id}`);
+  };
+
+  const handleLoad = () => {
+    setIsLoading(false);
   };
 
   return (
@@ -19,21 +32,35 @@ export default function RecipeCard({ recipe }: any) {
       radius="sm"
     >
       <CardBody className="p-0">
-        <Image src={recipe.coverImg} alt="recipe" isZoomed />
+        <Image
+          src={recipe.coverImg}
+          alt="recipe"
+          isZoomed
+          isLoading={isLoading}
+          onLoad={handleLoad}
+        />
       </CardBody>
-      <CardFooter className="flex-col gap-3">
-        <h1 className="w-full text-left text-md md:text-lg font-bold">{recipe.title}</h1>
+      <CardFooter className="flex-col gap-3 items-start">
+        <Skeleton isLoaded={!isLoading} className="rounded-lg">
+          <h1 className="w-full text-left text-md md:text-lg font-bold">
+            {recipe.title}
+          </h1>
+        </Skeleton>
         <div className="flex justify-between w-full items-center">
-          <User
-            name={recipe.creator?.username}
-            avatarProps={{
+          <Skeleton isLoaded={!isLoading} className="rounded-lg">
+            <User
+              name={recipe.creator?.username}
+              avatarProps={{
                 className: "w-[1.2rem] h-[1.2rem]",
-            }}
-          />
-          <div className="flex gap-1 items-center text-sm">
-            <HeartIcon size={15} weight="fill" color="#DC143C" />
-            {recipe.likes > 999 ? "999+" : recipe.likes}
-          </div>
+              }}
+            />
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading} className="rounded-lg">
+            <div className="flex gap-1 items-center text-sm">
+              <HeartIcon size={15} weight="fill" color="#DC143C" />
+              {recipe.likes > 999 ? "999+" : recipe.likes}
+            </div>
+          </Skeleton>
         </div>
       </CardFooter>
     </Card>
