6a27175f539b49fc0b17183eac50c427e6c8254b
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-07-31 14:35:32 -0400

    feat: display skeleton if the data (e.g. image) is loaded yet

--- src/components/RecipeViewer/index.tsx
+++ src/components/RecipeViewer/index.tsx
@@ -1,21 +1,39 @@
+import { useState } from "react";
+
 import { Image, ScrollShadow } from "@heroui/react";
 import PostCard from "@components/PostCard";
 import StepCard from "./StepCard";
 
 export default function RecipeViewer({ recipe }: any) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <ScrollShadow className="size-full" hideScrollBar>
       <article className="w-full h-full flex flex-col gap-3">
-        <Image
-          src={recipe.coverImg}
-          alt={recipe.title}
-          className="object-cover"
-          width={"100%"}
-          height={250}
-          radius="none"
-        />
         <div>
-          <PostCard creator={recipe.creator} shadow="none" radius="none" className="p-1">
+          <Image
+            src={recipe.coverImg}
+            alt={recipe.title}
+            className="object-cover"
+            width={"100%"}
+            height={250}
+            radius="none"
+            onLoad={handleLoad}
+            isLoading={isLoading}
+          />
+        </div>
+        <div>
+          <PostCard
+            creator={recipe.creator}
+            shadow="none"
+            radius="none"
+            className="p-1"
+            isLoading={isLoading}
+          >
             {recipe.desc}
           </PostCard>
         </div>
