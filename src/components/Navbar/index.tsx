c6db26c83e65b07f729130babd4c2d9051599563
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-08-01 22:56:29 -0400

    feat: implement a top navigation bar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,72 @@
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react";
+import SiteBrand from "@assets/brand/brand.svg?react";
+import { CaretLeftIcon, DotsThreeIcon } from "@phosphor-icons/react";
+
+import { useLocation, useNavigate } from "react-router";
+
+function navbarSelector(location: string) {
+  const navbarMap = [
+    {
+      regex: /recipe\/.*/,
+      navbarContent: () => <RecipePageNavbarContent />,
+    },
+  ];
+
+  const matched = navbarMap.find(({ regex }) => regex.test(location));
+
+  return matched?.navbarContent() ?? <MainNavbarContent />;
+}
+
+function GoBackButton({ size }: any) {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate(-1);
+  };
+
+  return (
+    <button className="cursor-pointer" onClick={handleOnClick}>
+      <CaretLeftIcon weight="bold" size={size} />
+    </button>
+  );
+}
+
+function MoreButton({ size }: any) {
+  return (
+    <button className="cursor-pointer">
+      <DotsThreeIcon weight="bold" size={size} />
+    </button>
+  );
+}
+
+function MainNavbarContent() {
+  return (
+    <NavbarBrand>
+      <SiteBrand className="w-[150px]" />
+    </NavbarBrand>
+  );
+}
+
+function RecipePageNavbarContent() {
+  return (
+    <NavbarBrand>
+      <NavbarContent>
+        <NavbarItem className="flex items-center">
+          <GoBackButton size={20} />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="flex items-center">
+          <MoreButton size={27} />
+        </NavbarItem>
+      </NavbarContent>
+    </NavbarBrand>
+  );
+}
+
+export default function NavigationBar() {
+  const location = useLocation();
+  const content = navbarSelector(location.pathname);
+
+  return <Navbar className="border-b border-b-default-200">{content}</Navbar>;
+}
