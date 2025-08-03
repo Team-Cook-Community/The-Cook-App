33b46069d1d5e2727f338359ffc4ed44966f6c2d
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-08-03 01:28:45 -0400

    feat: create reusable form field components

---
+++ src/components/Fields/index.tsx
@@ -0,0 +1,11 @@
+import AutoTextarea, { ControlledTextareaField } from "./AutoTextarea";
+import ImageUploadField, {
+  ControlledImageUploadField,
+} from "./ImageUploadField";
+
+export {
+  AutoTextarea,
+  ImageUploadField,
+  ControlledImageUploadField,
+  ControlledTextareaField,
+};
