0d986ccd9b4ecdfe4dba3a9fbaaca26fddf4e594
Zhen Wei Liao <zhenwei.liao99@gmail.com>
2025-08-03 01:28:05 -0400

    feat: implement the add recipe form page

---
+++ src/components/AddRecipeForm/Fields/IngredientRowField.tsx
@@ -0,0 +1,31 @@
+import { useFormContext } from "react-hook-form";
+
+import { Button } from "@heroui/react";
+import { TrashIcon } from "@phosphor-icons/react";
+
+export default function IngredientRowField({ index, onRemove }: any) {
+  const { register } = useFormContext();
+
+  return (
+    <div className="flex justify-between gap-2 border-b-1 border-dashed!">
+      <input
+        className="w-1/2 py-2 outline-none"
+        placeholder="e.g. Angus Steak"
+        {...register(`ingredients.${index}.item`)}
+      />
+      <input
+        className="w-1/2 py-2  outline-none"
+        placeholder="e.g. 16 oz"
+        {...register(`ingredients.${index}.amount`)}
+      />
+      <Button
+        className="bg-transparent"
+        onPress={() => onRemove(index)}
+        title="Delete Ingredient"
+        isIconOnly
+      >
+        <TrashIcon size={20} weight="regular" />
+      </Button>
+    </div>
+  );
+}
